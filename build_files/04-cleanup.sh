84a29574439ff6ab7dac20fd68b7d19b308721b0
Ryan Walker <ryanwalker8484@gmail.com>
2025-06-06 16:40:56 -0400

    feat: initial commit

--- build_files/04-cleanup.sh
+++ build_files/04-cleanup.sh
@@ -4,7 +4,7 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-rm -rf /tmp/* || true
+# rm -rf /tmp/* || true
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 # find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
 
