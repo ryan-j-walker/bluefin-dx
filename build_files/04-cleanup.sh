780ecea2dca8eeeb1092fd7ea9fc68fef2a76461
Ryan Walker <ryanwalker8484@gmail.com>
2025-06-06 18:07:01 -0400

    feat: initial commit

--- build_files/04-cleanup.sh
+++ build_files/04-cleanup.sh
@@ -3,7 +3,7 @@
 echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
-flatpak uninstall --delete-data org.mozilla.Thunderbird
+echo $(flatpak list)
 # rm -rf /tmp/* || true
 # find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 # find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
